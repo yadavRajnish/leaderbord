7a45723665aabb7f419de6390db1d0b83ed66699
Rajnish Yadav <rajnishyadav5509@gmail.com>
2025-01-09 16:49:59 +0530

    add: export csv

--- backend/controller/user.controller.ts
+++ backend/controller/user.controller.ts
@@ -30,6 +30,46 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
   }
 };
 
+export const getAllUserToCSV = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const data = await User.findAll();
+
+    if (!data || data.length === 0) {
+      res.status(400).send({
+        message: "No users found!"
+      })
+      return;
+    }
+
+    const header = ["Name", "Phone", "Email", "Laptime", "Created At", "Updated At"];
+    const csvRows = [];
+    csvRows.push(header.join(','));
+
+    data.forEach((user: any) => {
+      const row = [
+        user.name,
+        user.phone,
+        user.email,
+        user.laptime,
+        user.createdAt,
+        user.updatedAt
+      ];
+      csvRows.push(row.join(","));
+    })
+
+    const csvContent = csvRows.join("\n")
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachmen ; filename=player.csv")
+    res.send(csvContent);
+
+  } catch (error: any) {
+    res.status(500).send({
+      message: error.message || "Some error occurred while retrieving users.",
+    });
+  }
+}
+
+
 //get the user by id
 export const getUserById = async (req: Request, res: Response): Promise<void> => {
   const id = req.params.id;
--- backend/routes/user.router.ts
+++ backend/routes/user.router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addUSer, getAllUsers, getUserById } from '../controller/user.controller';
+import { addUSer, getAllUsers, getAllUserToCSV, getUserById } from '../controller/user.controller';
 
 const router = express.Router();
 
 router.post('/add-user', addUSer);
 router.get('/get-all-users', getAllUsers);
+router.get('/export-user-csv', getAllUserToCSV);
+
 router.get('/get-user-by-id/:id', getUserById);
 
 export default router;
